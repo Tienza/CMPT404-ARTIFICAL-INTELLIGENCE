4fe27efdd9aa98cbb98dc2d5f96c3aaa7ab1a5c0
Tienza <Piradon.Liengtiraphan1@gmail.com>
2016-09-05 13:33:34 -0400

    Installed all required packages and took a screenshot to prove that it is running in the machine

---
+++ Homework 0/Python Proof/python_proof.py
@@ -0,0 +1,21 @@
+import sys
+import numpy
+import scipy
+import sklearn
+import matplotlib
+import pandas
+
+print("import sys")
+print("import numpy")
+print("import scipy")
+print("import sklearn")
+print("import matplotlib")
+print("import pandas")
+print(" ")
+
+print("Python sys verion is " + sys.version)
+print("Python numpy version is " + numpy.__version__)
+print("Python scipy version is " + scipy.__version__)
+print("Python sklearn version is " + sklearn.__version__)
+print("Python matplotlib version is " + matplotlib.__version__)
+print("Python pandas version is " + pandas.__version__)
