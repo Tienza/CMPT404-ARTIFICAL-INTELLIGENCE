462f947302631c6493c1cff4b1ccbdcb57499e47
Tienza <Piradon.Liengtiraphan1@gmail.com>
2016-11-01 10:37:41 -0400

    Got code working and printing results to file, added write up to documents folder

--- Homework/Liengtiraphan-04/Python/nearest_neighbors_classification.py
+++ Homework/Liengtiraphan-04/Python/nearest_neighbors_classification.py
@@ -15,19 +15,23 @@ from sklearn.model_selection import KFold
 import time
 
 # read digits data & split it into X (training input) and y (target output)
-dataset = hw4.genDataSet(1000)#genfromtxt('features.csv', delimiter=' ')
-y = np.array(dataset)[:, 0]
+X, y, ytrue = hw4.genDataSet(1000)#genfromtxt('features.csv', delimiter=' ')
+
+X = X.reshape(len(X),1)
+y = y.reshape(len(y),1)
+ytrue = ytrue.reshape(len(ytrue),1)
+'''y = np.array(dataset)[:, 0]
 X = np.array(dataset)[:, 1:]
 y[y != 0] = -1  # rest of numbers are negative class
-y[y == 0] = +1  # number zero is the positive class
+y[y == 0] = +1  # number zero is the positive class'''
 
 bestk = []
 kc = 0
-for n_neighbors in range(1, 1000, 2):
-    kf = KFold(n_splits=3)
-    n_neighbors = 2
+for n_neighbors in range(1, 900, 2):
+    kf = KFold(n_splits=10)
+    #n_neighbors = 2
     kscore = []
-    k = 1
+    k = 0
     for train, test in kf.split(X):
         # print("%s %s" % (train, test))
         X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
@@ -35,10 +39,10 @@ for n_neighbors in range(1, 1000, 2):
         # time.sleep(100)
 
         # we create an instance of Neighbors Classifier and fit the data.
-        clf = neighbors.KNeighborsClassifier(n_neighbors, weights='distance')
+        clf = neighbors.KNeighborsRegressor(n_neighbors, weights='distance')
         clf.fit(X_train, y_train)
 
-        kscore.append(clf.score(X_test, y_test))
+        kscore.append(abs(clf.score(X_test, y_test)))
         # print kscore[k]
         k = k + 1
 
@@ -47,6 +51,34 @@ for n_neighbors in range(1, 1000, 2):
     print(bestk[kc])
     kc += 1
 
+
 # to do here: given this array of E_outs in CV, find the max, its
 # corresponding index, and its corresponding value of n_neighbors
-print(bestk)
+new_bestk = sorted(bestk, reverse=True)
+get_index = [new_bestk[0], new_bestk[1], new_bestk[2]]
+
+print(sorted(bestk, reverse=True))
+print("")
+for i in get_index:
+    print("Idex of " + str(i) + "in bestk: " + str(bestk.index(i)))
+    print("n_neighbors of " + str(i) + " in bestk: " + str(bestk.index(i) + (bestk.index(i) + 1)))
+    print("")
+print("")
+for i in range(3):
+    print("Rank " + str(i+1) + ". " "CV E_out: " + str(new_bestk[i]))
+
+# Write to File
+with open('hw4_results.txt', 'w') as f:
+    f.write(str(sorted(bestk, reverse=True)))
+    f.write("\n")
+    f.write("\n")
+    for i in get_index:
+        f.write("Index of " + str(i) + " in bestk: " + str(bestk.index(i)))
+        f.write("\n")
+        f.write("n_neighbors of " + str(i) + " in bestk: " + str(bestk.index(i) + (bestk.index(i) + 1)))
+        f.write("\n")
+        f.write("\n")
+    f.write("\n")
+    for i in range(3):
+        f.write("Rank " + str(i+1) + ". " "CV E_out: " + str(new_bestk[i]))
+        f.write("\n")
